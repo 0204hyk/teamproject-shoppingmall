9f739fbb126889d2e8b23b5a55f6dc2c7b728a6c
kang <kang@218.38.137.28>
2023-02-28 11:34:34 +0900

    commit

--- OnoreTeamProject/src/main/java/com/onore/project/mapper/Shop1Mapper.java
+++ OnoreTeamProject/src/main/java/com/onore/project/mapper/Shop1Mapper.java
@@ -2,12 +2,12 @@ package com.onore.project.mapper;
 
 import java.util.List;
 
-import com.onore.project.dto.ProductDTO;
+import com.onore.project.dto.ProductsDTO;
 import com.onore.project.dto.ReviewDTO;
 
 public interface Shop1Mapper {
 	
-	ProductDTO getDetail(Integer product_num);
+	ProductsDTO getDetail(Integer product_num);
 	
 	List<ReviewDTO> getProReview(Integer product_num);
 }
