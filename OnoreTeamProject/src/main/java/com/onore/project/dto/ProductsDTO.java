3167d289ddba8dfd7bc17d35c1d95ff0becfe84b
kang <kang@218.38.137.28>
2023-03-08 11:24:51 +0900

    commit

--- OnoreTeamProject/src/main/java/com/onore/project/dto/ProductsDTO.java
+++ OnoreTeamProject/src/main/java/com/onore/project/dto/ProductsDTO.java
@@ -1,5 +1,7 @@
 package com.onore.project.dto;
 
+import java.util.Date;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,8 +15,11 @@ public class ProductsDTO {
 	private Integer product_price;
 	private String product_info;
 	private String product_path;
-	private String product_thumbnail;
+	private String product_thumbnail_1;
+	private String product_thumbnail_2;
+	private String product_thumbnail_3;
 	private Integer product_views;
 	private Integer product_likes;
+	private Date product_date;
 
 }
