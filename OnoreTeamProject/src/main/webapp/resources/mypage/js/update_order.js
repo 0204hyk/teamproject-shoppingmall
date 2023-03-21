6af35d21e2269b91cffc38c1882f90b430785dd7
kang <kang@localhost>
2023-03-21 11:40:11 +0900

    commit

--- OnoreTeamProject/src/main/webapp/resources/mypage/js/update_order.js
+++ OnoreTeamProject/src/main/webapp/resources/mypage/js/update_order.js
@@ -6,7 +6,7 @@ for(i = 0; i < form.length; i++) {
 		e.preventDefault();
 	
 		if(window.confirm('정말로 환불 요청 하시겠습니까?')) {
-			form[i-1].action = "./order/updateStatus";
+			form[i-1].action = contextPath + "/order/updateStatus";
 			form[i-1].method = "POST";
 			form[i-1].submit();
 		}
